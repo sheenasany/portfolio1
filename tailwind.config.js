15a6456763a7882bd0c5351cb05ee4a18bb5071d
Sheena Sang <sheenasany@gmail.com>
2023-11-26 17:27:37 -0500

    tailwind starter config, font, responsive design customization

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+    },
+    fontFamily: {
+      Nunito: ["Nunito, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    }
+  },
+  plugins: [],
+}
+
