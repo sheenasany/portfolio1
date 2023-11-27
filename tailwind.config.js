18c6a0bc94226cf05238d0ba46446a3d497808b5
Sheena Sang <sheenasany@gmail.com>
2023-11-27 07:27:21 -0500

    fixed navbar color themes, added resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,19 +10,24 @@ module.exports = {
           'to': '#831843',
         },
         'about-colors': {
-          'from': '#fed7aa',
-          'via': '#fca5a5',
-          'to': '#4c1d95',
+          'from': '#134e4a',
+          'via': '#bef264',
+          'to': '#fb923c',
         },
         'project-colors': {
-          'from': '#134e4a',
-          'via': '#eab308',
-          'to': '#6ee7b7',
+          'from': '#610F40',
+          'via' : '#164e63',
+          'to': '#164e63',
         },
         'contact-colors': {
           'from': '#f97316',
           'via': '#7f1d1d',
           'to': '#7f1d1d'
+        },
+        'resume-colors': {
+          'from': '#8820A6',
+          'via': '#E67194',
+          'to': '#F7CC18'
         },
       },
     },
@@ -33,7 +38,7 @@ module.exports = {
       center: true,
       padding: "1rem",
       screens: {
-        xs: "320px",
+        // xs: "320px",
         lg: "1124px",
         xl: "1124px",
         "2xl": "1124px",
