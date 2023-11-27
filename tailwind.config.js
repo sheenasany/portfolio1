053f6d80d9d05d0542d5f5f8fe0e8f83abe3ef5b
Sheena Sang <sheenasany@gmail.com>
2023-11-27 02:32:13 -0500

    added gradient colors to theme and updated classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      gradientColorStops: {
+        'logo-colors': {
+          'from': '#f59e0b',
+          'via': '#115e59',
+          'to': '#831843',
+        },
+        'about-colors': {
+          'from': '#fed7aa',
+          'via': '#fca5a5',
+          'to': '#4c1d95',
+        },
+        'project-colors': {
+          'from': '#134e4a',
+          'via': '#eab308',
+          'to': '#6ee7b7',
+        },
+        'contact-colors': {
+          'from': '#f97316',
+          'via': '#7f1d1d',
+          'to': '#7f1d1d'
+        },
+      },
     },
     fontFamily: {
       Nunito: ["Nunito", "sans-serif"],
@@ -11,6 +33,7 @@ module.exports = {
       center: true,
       padding: "1rem",
       screens: {
+        xs: "320px",
         lg: "1124px",
         xl: "1124px",
         "2xl": "1124px",
